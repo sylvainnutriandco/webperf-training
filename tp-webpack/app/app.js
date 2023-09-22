c010ff11866e5e542965e0d4d6dad9e1e3a3a946
sylvainnutriandco <sn@nutriandco.com>
2023-09-22 15:50:05 +0200

    Add tp-webpack 2

--- tp-webpack/app/app.js
+++ tp-webpack/app/app.js
@@ -1,11 +1,15 @@
-// import loadable from '@loadable/component'
-import DatePicker from './component/date-picker.js'
-
-import Autocomplete from './component/autocomplete.js'
-
 import React from 'react'
-
 import ReactDOM from 'react-dom'
+import loadable from '@loadable/component'
+
+// import DatePicker from './component/date-picker.js'
+
+// import Autocomplete from './component/autocomplete.js'
+
+const DatePicker = loadable(() => import(/* webpackChunkName: "DatePicker" */ './component/date-picker.js'))
+
+const Autocomplete = loadable(() => import(/* webpackChunkName: "Autocomplete" */ './component/autocomplete.js'))
+
 
 ReactDOM.render(
 	<>
